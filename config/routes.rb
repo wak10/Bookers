a6df317387c25a3e142233c6e314091c9e1a6475
wak10 <taka.watagashi422@gmail.com>
2021-10-13 12:21:24 +0000

    [add]bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'homes/index'
+  get 'homes/show'
+  get 'homes/new'
+  get 'homes/edit'
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
