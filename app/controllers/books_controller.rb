5b69bfcb697c9a9e153b96eef1267373f6f79219
wak10 <taka.watagashi422@gmail.com>
2021-10-14 13:02:17 +0000

    [add]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,4 +12,16 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
 end
