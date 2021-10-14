7c1da33d4178f732897896e7b7e48e9b80727c68
wak10 <taka.watagashi422@gmail.com>
2021-10-14 13:52:23 +0000

    [add]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,13 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    @books= Book.all
   end
 
   def show
   end
 
   def new
-    @book = Book.new
+
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'books#new'
-  post '/' =>'books#top'
-
-  get 'top' =>'books#top'
+  root to: 'books#top'
 
   get 'index' => 'books#index'
 
